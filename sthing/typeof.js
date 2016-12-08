159af06c57d873ff91790cba3e914b34db9af062
kexiao <xk_xiaoke@163.com>
2016-12-08 18:30:33 +0800

    push

--- sthing/typeof.js
+++ sthing/typeof.js
@@ -2,9 +2,41 @@
  * Created by kexiao on 16/12/4.
  */
 
+
+// JS 变量 字符串、数字、布尔、数组、对象、Null、Undefined、Symbol
+
+var obj ={};
+
 var array =[1,2,3,5];
 
-console.log(array.constructor);
+var flag =true;
+
+var num =123;
+
+var unfid =undefined;
+
+var nu =null;
+
+var str ='hello';
+
+var sym = Symbol('');
+
+console.log(Object.prototype.toString.call(obj));
+
+console.log(Object.prototype.toString.call(array));
+
+console.log(Object.prototype.toString.call(flag));
+
+
+console.log(Object.prototype.toString.call(num));
+
+console.log(Object.prototype.toString.call(unfid));
+
+console.log(Object.prototype.toString.call(nu));
+
+console.log(Object.prototype.toString.call(str));
+
+console.log(Object.prototype.toString.call(sym));
 
 console.log(false == '0')
 console.log(false === '0')
@@ -30,6 +62,7 @@ var hero = {
 var stoleSecretIdentity = hero.getSecretIdentity;
 
 console.log(stoleSecretIdentity.call(hero));
+
 console.log(hero.getSecretIdentity());
 
 
