92636867a1d210251ffd7b3c77adf6dca99a2c4c
kexiao <xk_xiaoke@163.com>
2017-02-07 23:17:30 +0800

    new

---
+++ new/index.js
@@ -0,0 +1,16 @@
+/**
+ * Created by kexiao on 17/2/7.
+ */
+
+
+var ov = new function(){
+    return '圆心';
+}
+
+console.log(ov);
+
+var ov1 = new function(){
+    return new String('圆心');
+}
+
+console.log(ov1);
